ba558910cd67e3cf7345efada5006991e7e4e665
Anna Beatriz <annab_oliver@gmail.com>
2022-05-20 22:33:01 -0300

    Finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int a,b, resto;
+
+Console.WriteLine("Máximo Divisor Comum (método iterativo)");
+
+Console.Write("Digite o 1º número (a): ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o 2º número (b): ");
+b = Convert.ToInt32(Console.ReadLine());
+
+do
+{
+    resto = a % b;
+    a = b;
+    b = resto;
+}
+while (resto != 0);
+
+Console.WriteLine($"MDC(a, b) = {a}");
